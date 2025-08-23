2a4f41c11aaecdbd2f81c4e3b1a389dc12557d17
Hao Mi <mihao_2002@hotmail.com>
2025-08-23 10:01:22 -0700

    add loading percentage

--- Assets/Scripts/UI/Home.cs
+++ Assets/Scripts/UI/Home.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using System;
+using System.Collections;
 
 public class Home : MonoBehaviour
 {
@@ -19,6 +20,34 @@ public class Home : MonoBehaviour
     // Public method to load a scene by name
     public void LoadSceneByName(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false; // wait until we're ready
+
+        while (!operation.isDone)
+        {
+            // operation.progress goes from 0 → 0.9
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            // progressBar.value = progress;
+            buildProgressText.text = (progress * 100f).ToString("F0") + "%";
+
+            // once loading reaches 90% we can allow activation
+            if (operation.progress >= 0.9f)
+            {
+                // progressText.text = "Press any key to continue";
+                // progressBar.value = 1f;
+
+                // if (Input.anyKeyDown) // or a button press
+                // {
+                    operation.allowSceneActivation = true;
+                // }
+            }
+
+            yield return null;
+        }
     }
 }
