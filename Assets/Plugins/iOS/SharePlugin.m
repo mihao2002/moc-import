11ca665db11a9c72a90f637d4be758ca0d5ed7d1
Hao Mi <mihao_2002@hotmail.com>
2025-08-27 07:26:31 -0700

    add ios plugin

---
+++ Assets/Plugins/iOS/SharePlugin.m
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+extern "C" {
+    void _ShowShareSheet(const char* filePath)
+    {
+        NSString* path = [NSString stringWithUTF8String:filePath];
+        NSURL* fileURL = [NSURL fileURLWithPath:path];
+
+        UIActivityViewController* activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[fileURL] applicationActivities:nil];
+
+        UIViewController* rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [rootVC presentViewController:activityVC animated:YES completion:nil];
+    }
+}
